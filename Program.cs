44ac9059b6ab9bfdda0cc024f6a35be299945a44
shubhampachghare <shubhampachghare@gmail.com>
2023-02-25 00:16:57 +0530

    [Shubham]-CheckEqaulityofTwoLines

--- Program.cs
+++ Program.cs
@@ -1,17 +1,26 @@
 ﻿using System;
 
-namespace LineComparison
+namespace LineComparisonProblems
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int x1, x2, y1, y2;
             Console.WriteLine("Enter the value of (x1,y1),(x2,y2) cordinates");
-            int x1 = Convert.ToInt32(Console.ReadLine());
-            int x2 = Convert.ToInt32(Console.ReadLine());
-            int y1 = Convert.ToInt32(Console.ReadLine());
-            int y2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(toFindLength(x1, y1, x2, y2));
+            x1 = Convert.ToInt32(Console.ReadLine());
+            x2 = Convert.ToInt32(Console.ReadLine());
+            y1 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+            int length1 = toFindLength(x1, y1, x2, y2);
+
+            x1 = Convert.ToInt32(Console.ReadLine());
+            x2 = Convert.ToInt32(Console.ReadLine());
+            y1 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+            int length2 = toFindLength(x1, y1, x2, y2);
+
+            Console.WriteLine(length1.Equals(length2));
 
         }
 
